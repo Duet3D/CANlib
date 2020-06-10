f930f83a40ebd4fd14fbf02cc060112601ad3394
David Crocker <dcrocker@eschertech.com>
2020-06-10 11:56:26 +0100

    Renamed Duet3Limits.h to Duet3Common.h, added closed loop mode defs

--- src/Duet3Common.h
+++ src/Duet3Common.h
@@ -7,12 +7,13 @@
  *      Author: David
  */
 
-#ifndef SRC_DUET3LIMITS_H_
-#define SRC_DUET3LIMITS_H_
+#ifndef SRC_DUET3COMMON_H_
+#define SRC_DUET3COMMON_H_
 
 #include <cstdint>
 #include <cstddef>
 
+// Limits of Duet 3 systems
 constexpr size_t MaxSensors = 56;						// limited by the size of bitmap we can store in an ExpressionValue
 constexpr size_t MaxHeaters = 32;
 constexpr size_t MaxMonitorsPerHeater = 3;
@@ -28,4 +29,16 @@ constexpr size_t MaxGpOutPorts = 32;					// increased as requested by Jimmykc
 constexpr size_t MaxGpInPorts = 16;
 constexpr size_t MaxSpindles = 2;						// maximum number of configurable spindles
 
-#endif /* SRC_DUET3LIMITS_H_ */
+// Other constants etc. that are common across Duet 3 main and expansion boards
+
+// The values of this enumeration must correspond to the meanings of the M569.1 S parameter
+enum class ClosedLoopDriverMode : uint8_t
+{
+	openLoop = 0,
+	axisQuadrature,
+	motorQuadrature,
+	as5047,
+	tli5012
+};
+
+#endif /* SRC_DUET3COMMON_H_ */
