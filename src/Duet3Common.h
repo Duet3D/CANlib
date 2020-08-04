7c0a1a8402f6943d8423c26f2ee5854343169eca
David Crocker <dcrocker@eschertech.com>
2020-08-04 09:56:23 +0100

    Added a missing #include

--- src/Duet3Common.h
+++ src/Duet3Common.h
@@ -12,6 +12,7 @@
 
 #include <cstdint>
 #include <cstddef>
+#include <General/NamedEnum.h>
 
 // Limits of Duet 3 systems
 constexpr size_t MaxSensors = 56;						// limited by the size of bitmap we can store in an ExpressionValue
@@ -32,13 +33,6 @@ constexpr size_t MaxSpindles = 2;						// maximum number of configurable spindle
 // Other constants etc. that are common across Duet 3 main and expansion boards
 
 // The values of this enumeration must correspond to the meanings of the M569.1 S parameter
-enum class ClosedLoopDriverMode : uint8_t
-{
-	openLoop = 0,
-	axisQuadrature,
-	motorQuadrature,
-	as5047,
-	tli5012
-};
+NamedEnum(EncoderType, uint8_t, none, linearQuadrature, rotaryQuadrature, as5047, tli5012);
 
 #endif /* SRC_DUET3COMMON_H_ */
