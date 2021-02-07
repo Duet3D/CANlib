edbe2ec9e463841d71e234917c2d31855547e7ae
David Crocker <dcrocker@eschertech.com>
2021-02-07 10:26:14 +0000

    Added option to return board unique ID in CanMessageReturnInfo

--- src/CanMessageFormats.h
+++ src/CanMessageFormats.h
@@ -265,6 +265,7 @@ struct __attribute__((packed)) CanMessageReturnInfo
 	static constexpr uint8_t unused_was_typePressureAdvance = 2;
 	static constexpr uint8_t typeM408 = 3;
 	static constexpr uint8_t typeBootloaderName = 4;
+	static constexpr uint8_t typeBoardUniqueId = 5;
 	static constexpr uint8_t typeDiagnosticsPart0 = 100;
 	// Other parts of the diagnostics reply use 101, 102 etc. so keep these free
 
