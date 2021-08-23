05fff61155ed3e1b08158a8bd351bbd587faa6c9
Louis Irwin <coding@louisirwin.co.uk>
2021-08-23 16:39:05 +0100

    Closed loop control (#3)
    * Added efficient way of calculating set bits to closed loop CAN message
    
    * Fixed closed loop merge conflicts
    
    * Moved closed-loop recording consts to shared CANlib file

--- src/CanMessageFormats.h
+++ src/CanMessageFormats.h
@@ -1035,29 +1035,17 @@ struct __attribute__((packed)) CanMessageClosedLoopData
 	uint16_t zero2;							// for alignment, currently spare
 	float    data[14];
 
-	// Get the actual amount of data
-	size_t GetActualDataLength() const noexcept
+	// Count how many bits are set in filter (i.e. how many variables are being collected)
+	size_t GetFilterSetBits() const noexcept
 	{
 		Bitmap<uint16_t> tmpFilter(filter);
-		return sizeof(uint32_t) + sizeof(uint16_t) + numSamples * tmpFilter.CountSetBits() * sizeof(float);
+		return tmpFilter.CountSetBits();
 	}
-};
-
-// Message used to send logging data from an expansion board to the master
-struct __attribute__((packed)) CanMessageLogMessage
-{
-	static constexpr CanMessageType messageType = CanMessageType::logMessage;
-
-	bool lastPacket;		// Are there more packets to come?
-	time_t time;			// Time of the message
-	uint8_t type;			// The type of logging message
-	uint16_t packetLength;	// Character count of the message
-	char message[50];		// The message
 
 	// Get the actual amount of data
 	size_t GetActualDataLength() const noexcept
 	{
-		return sizeof(bool) + sizeof(time_t) + sizeof(uint8_t) + sizeof(uint16_t) + packetLength * sizeof(char);
+		return sizeof(uint32_t) + sizeof(uint16_t) + numSamples * GetFilterSetBits() * sizeof(float);
 	}
 };
 
@@ -1120,7 +1108,6 @@ union CanMessage
 	CanMessageAccelerometerData accelerometerData;
 	CanMessageStartClosedLoopDataCollection startClosedLoopDataCollection;
 	CanMessageClosedLoopData closedLoopData;
-	CanMessageLogMessage canMessageLogMessage;
 };
 
 static_assert(sizeof(CanMessage) <= 64, "CAN message too big");		// check none of the messages is too large
