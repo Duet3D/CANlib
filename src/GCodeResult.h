35306ea394124538653f7f9bb791dd0d3fb9b9c3
David Crocker <dcrocker@eschertech.com>
2022-01-13 09:42:28 +0000

    Added Succeeded(GCodeResult) function

--- src/GCodeResult.h
+++ src/GCodeResult.h
@@ -8,15 +8,15 @@
 #ifndef SRC_GCODERESULT_H_
 #define SRC_GCODERESULT_H_
 
-#include <cctype>
+#include <cstdint>
 
 // Enumeration to specify the result of attempting to process a GCode command
 enum class GCodeResult : uint8_t
 {
 	notFinished,					// we haven't finished processing this command
 	ok,								// we have finished processing this code in the current state, and if the GCodeState is 'normal' then we have finished it completely
-	warning,
-	warningNotSupported,
+	warning,						// the command succeeded but a warning was generated
+	warningNotSupported,			// the command is not supported, but for this command we issue a warning not an error
 	error,
 	errorNotSupported,
 	notSupportedInCurrentMode,
@@ -24,20 +24,26 @@ enum class GCodeResult : uint8_t
 	remoteInternalError
 };
 
+// Test whether the command succeeded
+inline constexpr bool Succeeded(GCodeResult rslt) noexcept
+{
+	return rslt == GCodeResult::ok || rslt == GCodeResult::warning;
+}
+
 // Convert a true/false error/no-error indication to a GCodeResult
-inline GCodeResult GetGCodeResultFromError(bool err)
+inline constexpr GCodeResult GetGCodeResultFromError(bool err) noexcept
 {
 	return (err) ? GCodeResult::error : GCodeResult::ok;
 }
 
 // Convert a true/false success/failure indication to a GCodeResult
-inline GCodeResult GetGCodeResultFromSuccess(bool ok) noexcept
+inline constexpr GCodeResult GetGCodeResultFromSuccess(bool ok) noexcept
 {
 	return (ok) ? GCodeResult::ok : GCodeResult::error;
 }
 
 // Convert a true/false finished/not-finished indication to a GCodeResult
-inline GCodeResult GetGCodeResultFromFinished(bool finished)
+inline constexpr GCodeResult GetGCodeResultFromFinished(bool finished) noexcept
 {
 	return (finished) ? GCodeResult::ok : GCodeResult::notFinished;
 }
