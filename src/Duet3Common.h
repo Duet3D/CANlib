372281cfca4b76578fbb27138a3dbcb709cfcabe
David Crocker <dcrocker@eschertech.com>
2022-01-17 19:19:47 +0000

    Increased maximum GpIn ports on Duet 3 series

--- src/Duet3Common.h
+++ src/Duet3Common.h
@@ -27,7 +27,7 @@ constexpr size_t MaxFans = 20;
 constexpr size_t MaxGpOutPorts = 32;						// increased as requested by Jimmykc
 
 // The following currently don't need to be known by expansion boards, but might in future
-constexpr size_t MaxGpInPorts = 16;
+constexpr size_t MaxGpInPorts = 32;
 constexpr size_t MaxSpindles = 4;							// maximum number of configurable spindles
 
 constexpr uint32_t ActLedFlashTime = 100;					// how long the ACT LED stays on after we process a CAN message
