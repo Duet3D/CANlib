d2210cd9cdde4e40c4c575b273b58ddc75600b91
David Crocker <dcrocker@eschertech.com>
2022-10-20 16:15:40 +0100

    Changed type of M569.1 S parameter

--- src/CanMessageGenericTables.h
+++ src/CanMessageGenericTables.h
@@ -94,7 +94,7 @@ constexpr ParamDescriptor M569Point1Params[] =
 	FLOAT_PARAM('I'),
 	FLOAT_PARAM('D'),
 	FLOAT_PARAM('H'),		// minimum holding current percent when in closed loop mode
-	FLOAT_PARAM('S'),		// steps/rev added for EXP1HCL firmware 3.5 compatibility
+	UINT16_PARAM('S'),		// steps/rev added for EXP1HCL firmware 3.5 compatibility
 	END_PARAMS
 };
 
