78b0375121f1868f0f66ab3c3b439080b25fa529
David Crocker <dcrocker@eschertech.com>
2025-01-10 18:11:30 +0000

    Reworked RemoteInputHandle to make functions constexpr

--- src/RemoteInputHandle.h
+++ src/RemoteInputHandle.h
@@ -9,26 +9,25 @@
 #define SRC_REMOTEINPUTHANDLE_H_
 
 // Type used to represent a handle to a remote input
-struct __attribute__((packed)) RemoteInputHandle
+union __attribute__((packed)) RemoteInputHandle
 {
-	RemoteInputHandle(uint8_t p_type, uint8_t p_major, uint8_t p_minor) noexcept { Set(p_type, p_major, p_minor); }
-	RemoteInputHandle() noexcept { u.all = 0; }		// constructor sets it to invalid
-	void Set(uint8_t p_type, uint8_t p_major, uint8_t p_minor) noexcept { u.parts.type = p_type; u.parts.major = p_major; u.parts.minor = p_minor; }
-	void Set(uint16_t p_all) noexcept { u.all = p_all; }
-	uint16_t asU16() const noexcept { return u.all; }
-	bool IsValid() const noexcept { return u.parts.type > typeUnset && u.parts.type < lowestBadType; }
-	bool operator==(RemoteInputHandle other) const noexcept { return u.all == other.u.all; }
-
-	union
+	struct __attribute__((packed)) HandleParts
 	{
-		struct
-		{
-			uint16_t minor : 6,						// endstop switch number within axis (for endstops)
-					major : 6,						// axis number (for endstops), or GPIn number, or Z probe number
-					type : 4;						// what the handle is used for, see list of types below
-		} parts;
-		uint16_t all;
-	} u;
+		uint16_t minor : 6,						// endstop switch number within axis (for endstops)
+				major : 6,						// axis number (for endstops), or GPIn number, or Z probe number
+				type : 4;						// what the handle is used for, see list of types below
+		constexpr HandleParts(uint8_t p_type, uint8_t p_major, uint8_t p_minor): minor(p_minor), major(p_major), type(p_type)  { }
+	} parts;
+	uint16_t all;
+
+	constexpr RemoteInputHandle() : all(0) { }
+	constexpr RemoteInputHandle(uint8_t p_type, uint8_t p_major, uint8_t p_minor) : parts(p_type, p_major, p_minor) { }
+
+	constexpr void Set(uint8_t p_type, uint8_t p_major, uint8_t p_minor) noexcept { parts.type = p_type; parts.major = p_major; parts.minor = p_minor; }
+	constexpr void Set(uint16_t p_all) noexcept { all = p_all; }
+	constexpr uint16_t asU16() const noexcept { return all; }
+	constexpr bool IsValid() const noexcept { return parts.type > typeUnset && parts.type < lowestBadType; }
+	constexpr bool operator==(RemoteInputHandle other) const noexcept { return all == other.all; }
 
 	static constexpr uint16_t typeUnset = 0, typeEndstop = 1, typeGpIn = 2, typeZprobe = 3, typeAte = 4, typeStallEndstop = 5, lowestBadType = 6;
 };
