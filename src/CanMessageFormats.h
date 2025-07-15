de1d84c0bf9d3bd0fc8304266a82a667b3b32c32
David Crocker <dcrocker@eschertech.com>
2025-07-15 14:38:47 +0100

    Removed CanMessageCreateInputMonitorOld

--- src/CanMessageFormats.h
+++ src/CanMessageFormats.h
@@ -441,44 +441,6 @@ struct __attribute__((packed)) CanMessageSetFanSpeed
 	void SetRequestId(CanRequestId rid) noexcept { requestId = rid; zero = 0; }
 };
 
-#if 0
-
-// Request to create an input monitor
-struct __attribute__((packed)) CanMessageCreateInputMonitorOld
-{
-	static constexpr CanMessageType messageType = CanMessageType::createInputMonitorOld;
-
-	uint16_t requestId : 12,
-			 zero : 4;
-	RemoteInputHandle handle;
-	uint16_t threshold;			// analog threshold, or zero if digital
-	uint16_t minInterval;
-	char pinName[56];			// null terminated
-
-	void SetRequestId(CanRequestId rid) noexcept { requestId = rid; zero = 0; }
-	size_t GetActualDataLength() const noexcept { return 3 * sizeof(uint16_t) + sizeof(RemoteInputHandle) + Strnlen(pinName, sizeof(pinName)/sizeof(pinName[0])); }
-	size_t GetMaxPinNameLength(size_t dataLength) const noexcept { return dataLength - (3 * sizeof(uint16_t) + sizeof(RemoteInputHandle)); }
-};
-
-// Request to reconfigure an input monitor
-struct __attribute__((packed)) CanMessageChangeInputMonitorOld
-{
-	static constexpr CanMessageType messageType = CanMessageType::changeInputMonitorOld;
-
-	uint16_t requestId : 12,
-			 zero : 4;
-	RemoteInputHandle handle;
-	uint16_t param;
-	uint8_t action;
-
-	static constexpr uint8_t actionDontMonitor = 0, actionDoMonitor = 1, actionDelete = 2, actionChangeThreshold = 3, actionChangeMinInterval = 4,
-								actionReturnPinName = 5;
-
-	void SetRequestId(CanRequestId rid) noexcept { requestId = rid; zero = 0; }
-};
-
-#endif
-
 // Request to create an input monitor
 struct __attribute__((packed)) CanMessageCreateInputMonitorNew
 {
