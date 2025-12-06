a5493ca015736b6021dbd91fb2416b6bf91878ae
David Crocker <dcrocker@eschertech.com>
2025-12-06 19:41:59 +0000

    Added missing changes from 3.7-dev branch

--- src/CanSettings.h
+++ src/CanSettings.h
@@ -14,12 +14,13 @@
 #include <General/SimpleMath.h>
 
 // In the following structure, the time quantum is 1 cycle of the 48MHz CAN clock that is used on all types of Duet 3 expansion and tool board.
+// The tseg1 field excludes the 1-clock sync phase for historical reasons. We retain it for compatibility with existing date stored in NVRAM.
 // The CAN bit time is tseg1 + tseg2 + 1 time quanta, so for the default bit rate of 1Mbit/sec this must add up to 48.
 struct CanTiming
 {
 	uint16_t period;				// number of time quanta in 1 bit time, or 0xFFFF if this and the following fields have not been set
-	uint16_t tseg1;					// how far into the period the sample point is minus 1, minimum 1, maximum period-2
-	uint16_t jumpWidth;				// the (re)synchronisation jump width
+	uint16_t tseg1;					// how far into the bit period the sample point is (minimum 1, maximum period-2) less 1
+	uint16_t jumpWidth;				// the (re)synchronisation jump width. The maximum is (period - (tseg1 + 1) but we check that when we program the CAN peripheral.
 
 	static constexpr uint32_t ClockFrequency = 48'000'000;					// CAN clock used by all Duet 3 boards
 	static constexpr uint32_t DefaultCanBitRate = 1'000'000;
@@ -71,7 +72,7 @@ private:
 	uint16_t GetChecksum() const noexcept;
 	void UpdateChecksum() noexcept;
 
-	static constexpr uint16_t magic = 0x4321;	// the expected XOR of all 8 words
+	static constexpr uint16_t magic = 0x4321;	// the expected XOR of all eight 16-bit words
 
 	// Total 16 bytes
 	uint16_t canIdV1NotSet : 1,					// set if canAddress does not contain the CAN address to use
@@ -81,9 +82,14 @@ private:
 	uint8_t invertedCanAddress;					// the inverted CAN address of this board, or 0xFF if it has not been set
 	CanTiming timing;							// this is 6 bytes long
 	uint16_t spare1, spare2;					// make up to 14 bytes for future expansion
-	uint16_t checksum;							// checksum word to make
+	uint16_t checksum;							// checksum word to make the XOR of all eight 16-bit words the magic value
 };
 
 static_assert(sizeof(CanUserAreaData) == 16);
+
+
+// Where we store the CAN data
+constexpr uint32_t CanUserAreaDataOffset_SAME5x = 512 - sizeof(CanUserAreaData);
+constexpr uint32_t CanUserAreaDataOffset_SAMC21 = 256 - sizeof(CanUserAreaData);
 
 #endif /* SRC_CANTIMINGDATA_H_ */
