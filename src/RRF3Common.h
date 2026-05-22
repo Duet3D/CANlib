3c61de32ef3c4a137358b819ce9101100fb8888e
dc42 <dcrocker@duet3d.com>
2026-05-22 12:03:39 +0100

    Corrected high PWM error fault message

--- src/RRF3Common.h
+++ src/RRF3Common.h
@@ -81,7 +81,7 @@ constexpr float FanFeedForwardMultiplier = 0.7;			// how much we under- or over-
 constexpr float DefaultMaxHeatingFaultTime = 5.0;		// How many seconds we allow a heating fault to persist
 constexpr float DefaultMaxPwmFaultTime = 20.0;			// How many seconds we allow a heating excess pwm fault to persist
 constexpr float AllowedTemperatureDerivativeNoise = 0.12;	// How much fluctuation in the averaged temperature derivative we allow
-constexpr float PwmFaultLevel = 1.25;					// Raise a PWM fault if the	PWM divided by the expected WM exceeds this value
+constexpr float PwmFaultLevel = 1.3;					// Raise a PWM fault if the	PWM divided by the expected WM exceeds this value
 constexpr float MaxAmbientTemperature = 45.0;			// We expect heaters to cool to this temperature or lower when switched off
 constexpr float NormalAmbientTemperature = 25.0;		// The ambient temperature we assume - allow for the printer heating its surroundings a little
 constexpr float LowAmbientTemperature = 15.0;			// A lower ambient temperature that we assume when checking heater performance
@@ -333,8 +333,8 @@ constexpr const char *_ecv_array HeaterFaultText[] =
 	"failed to read sensor: ",						// the sensor error message will be appended
 	"temperature rising too slowly: ",				// "expected ... measured ..." will be appended
 	"exceeded allowed temperature excursion: ",		// "target ... actual ..." will be appended
-	"pwm higher than expected",						// "expected ... actual ..." will be appended
 	"",												// "monitor ... was triggered" will be appended
+	"high PWM: ",									// "expected ... actual ..." will be appended
 	"unknown error: "								// this is used if the parameter is not a valid heater fault type
 };
 
